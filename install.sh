40c132f811d0668f6086a3f31d729ac7a577381b
H3adHunt3er_704 <29995733+budaksyabu704@users.noreply.github.com>
2018-01-30 00:59:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -58,8 +58,12 @@ UPDATE AND UPGRADE PROCESS
 
 PLEASE WAIT TAKE TIME 1-5 MINUTE
 "
-sh -c 'echo "deb http://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list'
-wget -qO - http://www.webmin.com/jcameron-key.asc | apt-key add -
+
+# set repo
+wget -q -O /etc/apt/sources.list "https://samreysteven91.000webhostapp.com/Debian8/sources.list.debian8"
+wget "https://samreysteven91.000webhostapp.com/Debian8/dotdeb.gpg"
+cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg
+
 apt-get update;
 apt-get -y upgrade;
 apt-get -y install wget curl;
@@ -69,40 +73,18 @@ INSTALLER PROCESS PLEASE WAIT
 TAKE TIME 5-10 MINUTE
 "
 
-#install menu
-wget https://raw.githubusercontent.com/zero9911/a/master/script/menu
-wget https://raw.githubusercontent.com/zero9911/a/master/script/user-list
-wget https://raw.githubusercontent.com/zero9911/a/master/script/monssh
-wget https://raw.githubusercontent.com/zero9911/a/master/script/status
-mv menu /usr/local/bin/
-mv user-list /usr/local/bin/
-mv monssh /usr/local/bin/
-mv status /usr/local/bin/
-chmod +x  /usr/local/bin/menu
-chmod +x  /usr/local/bin/user-list
-chmod +x  /usr/local/bin/monssh
-chmod +x  /usr/local/bin/status
-cd
-
-# moth
-cd
-wget https://raw.githubusercontent.com/zero9911/a/master/script/motd
-mv ./motd /etc/motd
-
 # fail2ban & exim & protection
 apt-get -y install fail2ban sysv-rc-conf dnsutils dsniff zip unzip;
 wget https://github.com/jgmdev/ddos-deflate/archive/master.zip;unzip master.zip;
 cd ddos-deflate-master && ./install.sh
 service exim4 stop;sysv-rc-conf exim4 off;
 
-# webmin
-apt-get -y install webmin
-sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
 
-# ssh
-sed -i 's/#Banner/Banner/g' /etc/ssh/sshd_config
-sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
-wget -O /etc/issue.net "https://raw.githubusercontent.com/zero9911/a/master/script/banner"
+# setting port ssh
+sed -i '/Port 22/a Port 143' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port  90' /etc/ssh/sshd_config
+sed -i 's/Port 22/Port  22/g' /etc/ssh/sshd_config
+service ssh restart
 
 # dropbear
 apt-get -y install dropbear
@@ -110,6 +92,33 @@ wget -O /etc/default/dropbear "https://raw.githubusercontent.com/zero9911/a/mast
 echo "/bin/false" >> /etc/shells
 echo "/usr/sbin/nologin" >> /etc/shells
 cd
+
+#Upgrade to Dropbear 2016
+cd
+apt-get install zlib1g-dev
+wget https://samreysteven91.000webhostapp.com/Debian8/dropbear-2016.74.tar.bz2
+bzip2 -cd dropbear-2016.74.tar.bz2 | tar xvf -
+cd dropbear-2016.74
+./configure
+make && make install
+mv /usr/sbin/dropbear /usr/sbin/dropbear.old
+ln /usr/local/sbin/dropbear /usr/sbin/dropbear
+cd && rm -rf dropbear-2016.74 && rm -rf dropbear-2016.74.tar.bz2
+service dropbear restart
+
+# install vnstat gui
+cd /home/vps/public_html/
+wget https://samreysteven91.000webhostapp.com/Debian8/vnstat_php_frontend-1.5.1.tar.gz
+tar xf vnstat_php_frontend-1.5.1.tar.gz
+rm vnstat_php_frontend-1.5.1.tar.gz
+mv vnstat_php_frontend-1.5.1 vnstat
+cd vnstat
+sed -i "s/\$iface_list = array('eth0', 'sixxs');/\$iface_list = array('eth0');/g" config.php
+sed -i "s/\$language = 'nl';/\$language = 'en';/g" config.php
+sed -i 's/Internal/Internet/g' config.php
+sed -i '/SixXS IPv6/d' config.php
+cd
+
 
 # squid3
 apt-get -y install squid3
@@ -128,6 +137,56 @@ echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
 wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/cucuatok93/teamcck/master/vps.conf"
 sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
 cd
+
+#install PPTP
+apt-get -y install pptpd
+cat > /etc/ppp/pptpd-options <<END
+name pptpd
+refuse-pap
+refuse-chap
+refuse-mschap
+require-mschap-v2
+require-mppe-128
+ms-dns 8.8.8.8
+ms-dns 8.8.4.4
+proxyarp
+nodefaultroute
+lock
+nobsdcomp
+END
+
+cat > /etc/pptpd.conf <<END
+option /etc/ppp/pptpd-options
+logwtmp
+localip 10.1.0.1
+remoteip 10.1.0.5-100
+END
+
+cat >> /etc/ppp/ip-up <<END
+ifconfig ppp0 mtu 1400
+END
+mkdir /var/lib/premium-script
+/etc/init.d/pptpd restart
+
+# install mrtg
+wget -O /etc/snmp/snmpd.conf "https://samreysteven91.000webhostapp.com/Debian8/snmpd.conf"
+wget -O /root/mrtg-mem.sh "https://samreysteven91.000webhostapp.com/Debian8/mrtg-mem.sh"
+chmod +x /root/mrtg-mem.sh
+cd /etc/snmp/
+sed -i 's/TRAPDRUN=no/TRAPDRUN=yes/g' /etc/default/snmpd
+service snmpd restart
+snmpwalk -v 1 -c public localhost 1.3.6.1.4.1.2021.10.1.3.1
+mkdir -p /home/vps/public_html/mrtg
+cfgmaker --zero-speed 100000000 --global 'WorkDir: /home/vps/public_html/mrtg' --output /etc/mrtg.cfg public@localhost
+curl "https://samreysteven91.000webhostapp.com/Debian8/mrtg.conf" >> /etc/mrtg.cfg
+sed -i 's/WorkDir: \/var\/www\/mrtg/# WorkDir: \/var\/www\/mrtg/g' /etc/mrtg.cfg
+sed -i 's/# Options\[_\]: growright, bits/Options\[_\]: growright/g' /etc/mrtg.cfg
+indexmaker --output=/home/vps/public_html/mrtg/index.html /etc/mrtg.cfg
+if [ -x /usr/bin/mrtg ] && [ -r /etc/mrtg.cfg ]; then mkdir -p /var/log/mrtg ; env LANG=C /usr/bin/mrtg /etc/mrtg.cfg 2>&1 | tee -a /var/log/mrtg/mrtg.log ; fi
+if [ -x /usr/bin/mrtg ] && [ -r /etc/mrtg.cfg ]; then mkdir -p /var/log/mrtg ; env LANG=C /usr/bin/mrtg /etc/mrtg.cfg 2>&1 | tee -a /var/log/mrtg/mrtg.log ; fi
+if [ -x /usr/bin/mrtg ] && [ -r /etc/mrtg.cfg ]; then mkdir -p /var/log/mrtg ; env LANG=C /usr/bin/mrtg /etc/mrtg.cfg 2>&1 | tee -a /var/log/mrtg/mrtg.log ; fi
+cd
+
 
 # openvpn
 apt-get -y install openvpn
@@ -149,6 +208,111 @@ echo "dragon:369" | chpasswd
 echo "UPDATE AND INSTALL COMPLETE COMPLETE 99% BE PATIENT"
 rm $0;rm *.txt;rm *.tar;rm *.deb;rm *.asc;rm *.zip;rm ddos*;
 clear
+
+# install webmin
+cd
+wget "https://samreysteven91.000webhostapp.com/Debian8/webmin_1.801_all.deb"
+dpkg --install webmin_1.801_all.deb;
+apt-get -y -f install;
+sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
+rm /root/webmin_1.801_all.deb
+service webmin restart
+service vnstat restart
+
+
+# install badvpn
+wget -O /usr/bin/badvpn-udpgw "https://samreysteven91.000webhostapp.com/Debian8/badvpn-udpgw"
+if [ "$OS" == "x86_64" ]; then
+  wget -O /usr/bin/badvpn-udpgw "https://samreysteven91.000webhostapp.com/Debian8/badvpn-udpgw64"
+fi
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
+chmod +x /usr/bin/badvpn-udpgw
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
+
+
+#!/bin/bash
+
+# text gambar
+apt-get install boxes
+
+# color text
+cd
+rm -rf /root/.bashrc
+wget -O /root/.bashrc "https://samreysteven91.000webhostapp.com/Menu/.bashrc"
+
+# install lolcat
+sudo apt-get -y install ruby
+sudo gem install lolcat
+
+# download script
+cd
+wget -O /usr/bin/motd "https://samreysteven91.000webhostapp.com/Menu/motd"
+wget -O /usr/bin/benchmark "https://samreysteven91.000webhostapp.com/Menu/benchmark.sh"
+wget -O /usr/bin/speedtest "https://samreysteven91.000webhostapp.com/Menu/speedtest_cli.py"
+wget -O /usr/bin/ps-mem "https://samreysteven91.000webhostapp.com/Menu/ps_mem.py"
+wget -O /usr/bin/dropmon "https://samreysteven91.000webhostapp.com/Menu/dropmon.sh"
+wget -O /usr/bin/menu "https://samreysteven91.000webhostapp.com/Menu/menu.sh"
+wget -O /usr/bin/user-active-list "https://samreysteven91.000webhostapp.com/Menu/user-active-list.sh"
+wget -O /usr/bin/user-add "https://samreysteven91.000webhostapp.com/Menu/user-add.sh"
+wget -O /usr/bin/user-add-pptp "https://samreysteven91.000webhostapp.com/Menu/user-add-pptp.sh"
+wget -O /usr/bin/user-del "https://samreysteven91.000webhostapp.com/Menu/user-del.sh"
+wget -O /usr/bin/disable-user-expire "https://samreysteven91.000webhostapp.com/Menu/disable-user-expire.sh"
+wget -O /usr/bin/delete-user-expire "https://samreysteven91.000webhostapp.com/Menu/delete-user-expire.sh"
+wget -O /usr/bin/banned-user "https://samreysteven91.000webhostapp.com/Menu/banned-user.sh"
+wget -O /usr/bin/unbanned-user "https://samreysteven91.000webhostapp.com/Menu/unbanned-user.sh"
+wget -O /usr/bin/user-expire-list "https://samreysteven91.000webhostapp.com/Menu/user-expire-list.sh"
+wget -O /usr/bin/user-gen "https://samreysteven91.000webhostapp.com/Menu/user-gen.sh"
+wget -O /usr/bin/userlimit.sh "https://samreysteven91.000webhostapp.com/Menu/userlimit.sh"
+wget -O /usr/bin/userlimitssh.sh "https://samreysteven91.000webhostapp.com/Menu/userlimitssh.sh"
+wget -O /usr/bin/user-list "https://samreysteven91.000webhostapp.com/Menu/user-list.sh"
+wget -O /usr/bin/user-login "https://samreysteven91.000webhostapp.com/Menu/user-login.sh"
+wget -O /usr/bin/user-pass "https://samreysteven91.000webhostapp.com/Menu/user-pass.sh"
+wget -O /usr/bin/user-renew "https://samreysteven91.000webhostapp.com/Menu/user-renew.sh"
+wget -O /usr/bin/clearcache.sh "https://samreysteven91.000webhostapp.com/Menu/clearcache.sh"
+wget -O /usr/bin/bannermenu "https://samreysteven91.000webhostapp.com/Menu/bannermenu"
+cd
+
+#rm -rf /etc/cron.weekly/
+#rm -rf /etc/cron.hourly/
+#rm -rf /etc/cron.monthly/
+rm -rf /etc/cron.daily/
+wget -O /root/passwd "https://samreysteven91.000webhostapp.com/Menu/passwd.sh"
+chmod +x /root/passwd
+echo "01 23 * * * root /root/passwd" > /etc/cron.d/passwd
+
+echo "*/30 * * * * root service dropbear restart" > /etc/cron.d/dropbear
+echo "00 23 * * * root /usr/bin/disable-user-expire" > /etc/cron.d/disable-user-expire
+echo "0 */12 * * * root /sbin/reboot" > /etc/cron.d/reboot
+#echo "00 01 * * * root echo 3 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a" > /etc/cron.d/clearcacheram3swap
+echo "*/30 * * * * root /usr/bin/clearcache.sh" > /etc/cron.d/clearcache1
+
+cd
+chmod +x /usr/bin/motd
+chmod +x /usr/bin/benchmark
+chmod +x /usr/bin/speedtest
+chmod +x /usr/bin/ps-mem
+#chmod +x /usr/bin/autokill
+chmod +x /usr/bin/dropmon
+chmod +x /usr/bin/menu
+chmod +x /usr/bin/user-active-list
+chmod +x /usr/bin/user-add
+chmod +x /usr/bin/user-add-pptp
+chmod +x /usr/bin/user-del
+chmod +x /usr/bin/disable-user-expire
+chmod +x /usr/bin/delete-user-expire
+chmod +x /usr/bin/banned-user
+chmod +x /usr/bin/unbanned-user
+chmod +x /usr/bin/user-expire-list
+chmod +x /usr/bin/user-gen
+chmod +x /usr/bin/userlimit.sh
+chmod +x /usr/bin/userlimitssh.sh
+chmod +x /usr/bin/user-list
+chmod +x /usr/bin/user-login
+chmod +x /usr/bin/user-pass
+chmod +x /usr/bin/user-renew
+chmod +x /usr/bin/clearcache.sh
+chmod +x /usr/bin/bannermenu
+cd
 
 # restart service
 service ssh restart
